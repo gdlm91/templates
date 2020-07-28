681e73b6a8607cf427b6073238f8b2aeab920fcc
Gerardo Leal <gerard.leal91@gmail.com>
2020-07-28 21:09:50 +1000

    Initial commit

---
+++ src/cli.ts
@@ -0,0 +1,21 @@
+import { build } from 'gluegun'
+
+/**
+ * Create the cli and kick it off
+ */
+async function run(argv) {
+  // create a CLI runtime
+  const cli = build('templates')
+    .src(__dirname)
+    .plugins('./node_modules', { matching: 'templates-*', hidden: true })
+    .help() // provides default for help, h, --help, -h
+    .version() // provides default for version, v, --version, -v
+    .create()
+  // and run it
+  const toolbox = await cli.run(argv)
+
+  // send it back (for testing, mostly)
+  return toolbox
+}
+
+module.exports = { run }
